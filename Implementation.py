d120f2f45715980e2ac0566a5de5d7b009d2a185
FabianCh <fabianchauvin@gmail.com>
2019-03-05 12:55:35 +0100

    adding conditional entropy and mutual information

--- Implementation.py
+++ Implementation.py
@@ -18,11 +18,24 @@ def joint_entropy(joint_distribution):
 
 
 def conditional_entropy(joint_distribution):
-    pass
+    h = 0
+    for i in range(len(joint_distribution)):
+        pi = sum(joint_distribution[i])
+        for j in range(len(joint_distribution[1])):
+            p = joint_distribution[i][j]
+            h -= p * math.log2(p/pi)
+    return h
 
 
-def mutual_information():
-    pass
+def mutual_information(joint_distribution):
+    h = 0
+    for i in range(len(joint_distribution)):
+        pi = sum(joint_distribution[i])
+        for j in range(len(joint_distribution[1])):
+            pj = sum(joint_distribution[:][j])
+            p = joint_distribution[i][j]
+            h -= p * math.log2(p / (pi * pj))
+    return h
 
 
 def cond_joint_entropy():
